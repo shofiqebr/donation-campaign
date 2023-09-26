5b8e9735954a66681ac7ee3f3b8e31e8e71e2271
shofiq <shofiqebr@gmail.com>
2023-09-26 11:55:32 +0600

    added dynamic color

--- src/Header/HeroArea/Cards/Card.jsx
+++ src/Header/HeroArea/Cards/Card.jsx
@@ -1,8 +1,15 @@
 const Card = ({card}) => {
     const {id,Picture,Title,Category,Category_bg,Card_bg,Text_and_button_bg,Description,Price} = card || {}
+
+
+    const cardStyles = {
+        backgroundColor: Card_bg,};
+       
+     const categoryStyles = {
+            backgroundColor: Category_bg};
   return (
     <div>
-      <div className="card card-compact w-96 bg-base-100 shadow-xl">
+      <div className="card card-compact w-96 bg-base-100 shadow-xl style={cardStyles}">
         <figure>
           <img
             src={Picture}
@@ -11,7 +18,7 @@ const Card = ({card}) => {
         </figure>
         <div className="card-body">
           <h2 className="card-title">{Title}</h2>
-          <p>{Category}</p>
+          <p style={categoryStyles}>{Category}</p>
           
         </div>
       </div>
